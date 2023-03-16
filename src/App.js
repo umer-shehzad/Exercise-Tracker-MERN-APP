69e270f13e6289369b8208904fe64c5e65404a69
furqanarshad52 <furqanarshad52@gmail.com>
2023-03-16 15:27:56 +0500

    signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './components/home/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div>
       <Home />
+      <Signup/>
     </div>
   );
 }

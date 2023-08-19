1be129ea28f3e1abf7effa0385d497794d07cbf6
umer-shehzad <umer.work.96@gmail.com>
2023-08-19 15:26:11 +0500

    update Footer Component

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,24 +1,39 @@
 import React from 'react'
-import {  CDBBox } from 'cdbreact';
+import { CDBBox } from 'cdbreact';
 
 const Footer = () => {
-    return(
+    return (
         <div>
-            <CDBBox display="flex" flex="column" className="mx-auto py-5" style={{ width: '90%' }}>
-                    <CDBBox display="flex" justifyContent="between" className="flex-wrap">
+            <CDBBox
+                display="flex"
+                flex="column"
+                className="py-1"
+                style={{
+                    width: '100%', border: '1px solid', marginTop: '87px',
+                    backgroundColor: "black", color: 'white'
+                }}>
+                <CDBBox display="flex" justifyContent="between" className="flex-wrap">
                     <CDBBox>
-                        <a href="/" className="d-flex align-items-center p-0 text-dark">
-                        {/* <img alt="LOGO IMG" src="logo" width="30px" /> */}
-                        <span className="ml-3 h5 font-weight-bold">Exrecise Tracker App</span>
+                        <a
+                            href="/" className="d-flex align-items-center p-0 text-dark"
+                            style={{ textDecoration: 'none' }}
+                        >
+                            {/* <img alt="LOGO IMG" src="logo" width="30px" /> */}
+                            <span
+                                className="ml-3 h5 font-weight-bold"
+                                style={{ color: 'skyblue', marginLeft: '10px' }}
+                            >Exercise Tracker App - Info</span>
                         </a>
-                        <p className="my-3" style={{ width: '250px' }}>
-                        Description of our App
+                        <p className="my-1" style={{ width: '100%', height: '30px', marginLeft: '30px' }}>
+                            The Exercise Tracker Application is a web-based solution designed to help users track their exercise routines. It allows users to log their exercise.
                         </p>
                     </CDBBox>
-                    </CDBBox>
-                    <small className="text-center mt-5">&copy; Exercise Tracker APP, 2023. All rights reserved.</small>
                 </CDBBox>
-            </div>          
+                <small
+                    className="text-center mt-0"
+                >&copy; Exercise Tracker APP, 2023. All rights reserved.</small>
+            </CDBBox>
+        </div>
     )
 }
 export default Footer;
